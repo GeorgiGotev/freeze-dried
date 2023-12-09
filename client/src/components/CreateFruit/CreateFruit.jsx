7f1bb601a5f9583e7fc0883442d2e93977d3992b
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2023-12-09 23:27:12 +0200

    some validation on add products

--- client/src/components/CreateFruit/CreateFruit.jsx
+++ client/src/components/CreateFruit/CreateFruit.jsx
@@ -16,6 +16,61 @@ export default function CreateFruit() {
         onCreateFruitSubmit
     );
 
+    const titleValidator = () => {
+        if (values.name.length < 3 || values.name.length > 30) {
+          setErrors((state) => ({
+            ...state,
+            name: "Title should be between 3 and 30 characters!",
+          }));
+          return;
+        } else {
+          if (errors.name) {
+            setErrors((state) => ({ ...state, name: "" }));
+          }
+        }
+      };
+
+      const imageValidator = () => {
+        if (!values.imageUrl.match("^https?://")) {
+          setErrors((state) => ({
+            ...state,
+            imageUrl: "Image should be a link and should start with http:// or https://",
+          }));
+        } else {
+          if (errors.imageUrl) {
+            setErrors((state) => ({ ...state, imageUrl: "" }));
+          }
+        }
+      };
+
+      const descriptionValidator = () => {
+        if (values.description.length < 20 || values.description.length > 100) {
+          setErrors((state) => ({
+            ...state,
+            description: "Description should be between 20 and 100 characters!",
+          }));
+          return;
+        } else {
+          if (errors.description) {
+            setErrors((state) => ({ ...state, description: "" }));
+          }
+        }
+      };
+
+      const moreInfoValidator = () => {
+        if (values.moreInfo.length < 20 || values.moreInfo.length > 120) {
+          setErrors((state) => ({
+            ...state,
+            moreInfo: "More Info should be between 20 and 120 characters!",
+          }));
+          return;
+        } else {
+          if (errors.moreInfo) {
+            setErrors((state) => ({ ...state, moreInfo: "" }));
+          }
+        }
+      };
+
     return (
         <section className="contact_section layout_padding">
             <div className="container">
@@ -33,7 +88,9 @@ export default function CreateFruit() {
                                             value={values.name}
                                             onChange={changeHandler}
                                             placeholder="Banana"
+                                            onBlur={titleValidator}
                                         />
+                                        {errors.name && <p className={styles.errors}>{errors.name}</p>}
                                     </div>
                                     <div>
                                     <label className={styles.label} htmlFor="imageUrl">Image Url</label>
@@ -43,7 +100,9 @@ export default function CreateFruit() {
                                             value={values.imageUrl}
                                             onChange={changeHandler}
                                             placeholder="www.somephoto.com"
+                                            onBlur={imageValidator}
                                         />
+                                        {errors.imageUrl && <p className={styles.errors}>{errors.imageUrl}</p>}
                                     </div>
                                     <div>
                                     <label className={styles.label} htmlFor="description">Description</label>
@@ -53,7 +112,9 @@ export default function CreateFruit() {
                                             value={values.description}
                                             onChange={changeHandler}
                                             placeholder="Information about this fruit"
+                                            onBlur={descriptionValidator}
                                         />
+                                        {errors.description && <p className={styles.errors}>{errors.description}</p>}
                                     </div>
                                     <div>
                                     <label className={styles.label} htmlFor="moreInfo">More Info</label>
@@ -63,7 +124,10 @@ export default function CreateFruit() {
                                             value={values.moreInfo}
                                             onChange={changeHandler}
                                             placeholder="Specific information about this product"
+                                            onBlur={moreInfoValidator}
                                         />
+                                        {errors.moreInfo && <p className={styles.errors}>{errors.moreInfo}</p>}
+
                                     </div>
                                     <div className="mt-5">
                                         <button type="submit">send</button>
